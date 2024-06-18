3793a1230a2941240fb64f8da39676d14f3ce48a
Suhel <Suhelgupta1792@gmail.com>
2024-06-18 09:56:42 +0530

    added login json

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Route, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { userModel } from './login.model';
+import { firstValueFrom } from 'rxjs';
+
 
 @Component({
   selector: 'app-login',
@@ -12,20 +16,27 @@ import { CommonModule } from '@angular/common';
 })
 export class LoginComponent implements OnInit {
   error:string = ''
-  userName:string = ''
-  passowrd:string = ''
-  constructor(public router:Router) {
+  user:userModel = new userModel();
+  userData:userModel[] = []
+  constructor(public router:Router,public http:HttpClient) {
   }
 
   ngOnInit(): void {
+    this.getAllLoginData();
   }
 
   login() {
-    if(this.passowrd == 'p' && this.userName == "y") {
+    let user = this.userData.find(x=> x.userName == this.user.userName && x.password == this.user.password);
+
+    if(user) {
       this.router.navigateByUrl("/task")
     } else {
       this.error = "Login Fail"
     }
+  }
+
+  async getAllLoginData() {
+    this.userData = await firstValueFrom(this.http.get("../../assets/login.json")) as any;
   }
 
 }
---
+++ src/app/login/login.model.ts
@@ -0,0 +1,4 @@
+export class userModel {
+    userName?:string
+    password?:string
+}
